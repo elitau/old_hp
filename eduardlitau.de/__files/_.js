c42acf3d225592d59d8d187c2c20f859bd3c607c
Eduard Litau <eduard.litau@gmail.com>
2012-06-24 21:48:12 +0200

    Static html files from old mobile.me account

---
+++ eduardlitau.de/__files/_.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20090418
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('__files/_Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');applyEffects()}
