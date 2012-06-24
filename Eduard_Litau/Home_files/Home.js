8f198a0f83257f1f1a440cb7f98bf299b25ee675
Eduard Litau <eduard.litau@gmail.com>
2012-06-24 23:28:41 +0200

    Now it's really a copy from mobile.me web folder

---
+++ Eduard_Litau/Home_files/Home.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120624
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
